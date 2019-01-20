42c47fba6b5e5cf8bb5302a0eaa4051f3753ff15
Anden Acitelli <a.acitelli@gmail.com>
2019-01-20 14:33:51 -0500

    Implemented basic pathfinding algorithm

--- Scripts/canvas.js
+++ Scripts/canvas.js
@@ -5,6 +5,8 @@
     Make it so you can click and drag. This worked alright when I tried it, but I encountered weird behavior with the red and blue 
         blocks and I'm taking a mental break from that and working on other aspects of the project. 
 
+    Bug: If you click on an edge block, drag off of the canvas, then release, the block isn't updated. 
+
 */
 
 /* 
@@ -14,7 +16,8 @@
     2 = Blue (Start) 
     3 = Red (End)
 */
-var currentMode = 0;
+var currentDrawingMode = 0;
+var currentPathfindingAlgorithm = "basic";
 
 // These are global b/c otherwise I'd need to loop through every one every time
 // Necessary to check and make sure only one of these exists at once 
@@ -83,7 +86,7 @@ function addGridItemEventListeners(element)
 
 function colorElement(element)
 {
-    switch(currentMode)
+    switch(currentDrawingMode)
     {
         // If the user is in erase mode 
         case 0:
@@ -199,6 +202,11 @@ function wipeElemBackgroundColor(element)
         element.classList.toggle("blue");
         numBlue--;
     }
+
+    if (element.classList.contains("gray"))
+    {
+        element.classList.toggle("gray");
+    }
 }
 
 function removeItemsWithClass(className)
@@ -221,22 +229,22 @@ function initializeButtonEventListeners()
     elements make their decision based on the global variable. */
     document.getElementById("blueSquareButton").addEventListener("click", function()
     {
-        currentMode = 2;
+        currentDrawingMode = 2;
     });
 
     document.getElementById("redSquareButton").addEventListener("click", function()
     {
-        currentMode = 3;
+        currentDrawingMode = 3;
     });
 
     document.getElementById("blackSquareButton").addEventListener("click", function()
     {
-        currentMode = 1;
+        currentDrawingMode = 1;
     });
 
     document.getElementById("eraseButton").addEventListener("click", function()
     {
-        currentMode = 0;
+        currentDrawingMode = 0;
     });
 
     document.getElementById("wipeButton").addEventListener("click", function()
@@ -247,37 +255,280 @@ function initializeButtonEventListeners()
     // Physically starts pathfinding 
     document.getElementById("startButton").addEventListener("click", function()
     {
-        /* Todo - Implement this later once I finish all the grid setup stuff */
+        switch(currentPathfindingAlgorithm)
+        {
+            case "basic":
+            {
+                intuitivePathfinder();
+            }
+        }
     });
 }
 
-/* Pressing Logic: 
-
-    "mouseenter" Event:
-
-        If mouse is pressed, fire coloring event for the current square. 
-    
-    "click" Event:
-
-        Accounts for edge case with mouseenter where it won't do it for the current square 
-
-*/
-
-/* Todo - Fix bug where clicking on a border inverts the result of this */
+/* This code keeps track of whether the mouse is pressed or not - 
+    Necessary for dragging behavior to work correctly w/ the blue and red blocks */
 
 var mouseIsDown = false;
-
 document.onmousedown = function()
 {
     mouseIsDown = true;
-    console.debug("Mouse is currently down.");
+    // console.debug("Mouse is currently down.");
 };
 
 document.onmouseup = function()
 {
     mouseIsDown = false;
-    console.debug("Mouse is currently up.");
+    // console.debug("Mouse is currently up.");
 };
+
+// Tracks which blocks have been checked so that every iteration of the recursive function
+// doesn't recheck blocks and clog up an absolutely massive amount of resources
+var checkedIndexes = [];
+
+// Represents a "hard stop" to the recursion because it's at the start of every one 
+var endBlockFound = false;
+
+function intuitivePathfinder()
+{
+    // Resetting variables
+    endBlockFound = false;
+    checkedIndexes = [];
+
+    /* Algorithm Pseudocode:
+
+        1. Start at the start block.
+        2. Explore in each direction in the four cardinal directions (no diagonals).
+            Note: This process is blocked by a wall. 
+        3. Continue this process for each block, repeating ad nauseum
+        4. If the end block is found, then all pathfinding stops. 
+        5. If no end block is ever found, the maze is "impossible". 
+    */
+
+    // Getting every grid element
+    // Note - These are returned in document order 
+    let gridItems = document.querySelectorAll(".gridItem");
+
+    // Starts with a debug value so if the user doesn't put a start block, it's diagnosable
+    let startBlockIndex = getStartBlockIndex(gridItems);
+
+    if (startBlockIndex === -1)
+    {
+        alert("No start block detected.");
+        return 0;
+    }
+
+    let endBlockIndex = getEndBlockIndex(gridItems);
+
+    if (endBlockIndex === -1)
+    {
+        alert("No end block detected.");
+        return 0;
+    }
+
+    /* Starts the actual pathfinding process */  
+    checkSurroundings(gridItems, startBlockIndex); 
+}
+
+function checkSurroundings(gridItems, currIndex)
+{
+    console.debug("Current Index: " + currIndex);
+
+    // This might not even be necessary but it's sorta here for safety 
+    if (endBlockFound)
+    {
+        console.log("End block has been found. Ceasing operations.");
+        return 0;
+    }
+
+    // If this block was already checked, nip it off at the bud to save computation time 
+    // This could technically be checked before it's even called, but the performance
+    //      difference is negligible and this is seemingly easier for me to implement 
+    if (checkedIndexes.includes(currIndex))
+    {        
+        console.log("Current block has already been checked.");
+        return 0;
+    }
+
+    console.debug("Index " + currIndex + " is non-checked.");
+
+    // Making this a checked index and shading it in to signify so 
+    checkedIndexes.push(currIndex);
+    gridItems[currIndex].classList.toggle("gray");
+
+    // * Checking for the goal 
+
+    // ! Todo - Make it alternate directions so it doesn't always just check left 
+    // Making sure it's not in the left column 
+    // CHecking to make sure it's not the first element in the row 
+    if (currIndex % 20 !== 0)
+    {
+        // This check has to be nested, otherwise an out of bounds error will occur 
+        if (gridItems[currIndex - 1].classList.contains("red"))
+        {            
+            console.debug("End block has been found.");
+            endBlockFound = true;
+            return 0;
+        }
+    }
+
+    // Making sure it's not in the right column 
+    if ((currIndex + 1) % 20 !== 0)
+    {        
+        // Checking right 
+        if (gridItems[currIndex + 1].classList.contains("red"))
+        {
+            console.debug("End block has been found.");
+            endBlockFound = true;
+            return 0;
+        }
+    }
+
+    // Making sure it's not in the top row 
+    if(!(currIndex < 20))
+    {        
+        // Checking up 
+        if (gridItems[currIndex - 20].classList.contains("red"))
+        {
+            console.debug("End block has been found.");
+            endBlockFound = true;
+            return 0;
+        }
+    }
+
+    // Making sure it's not in the last row 
+    if (!(currIndex >= 380))
+    {
+        // Checking down 
+        if (gridItems[currIndex + 20].classList.contains("red"))
+        {
+            console.debug("End block has been found.");
+            endBlockFound = true;
+            return 0;
+        }
+    }
+
+    // * Checking if surrounding blocks are valid, and if so, running this function again recursively
+
+    // Checking Left - Similar logic to above
+    if (currIndex % 20 !== 0)
+    {
+        if (gridItems[currIndex - 1].classList.contains("black"))
+        {
+            console.debug("Left block is a wall.");
+            checkedIndexes.push(currIndex - 1);
+        }
+
+        else 
+        {
+            console.debug("Checking left block.");
+            setTimeout(checkSurroundings, 100, gridItems, currIndex - 1);
+        }
+    }
+
+    else 
+    {
+        console.debug("Left block is in the leftmost column.");
+    }
+
+    // Checking Right  - Similar logic to above
+    // Making sure it's not in the right column 
+    if ((currIndex + 1) % 20 !== 0)
+    {        
+        // Checking right 
+        if (gridItems[currIndex + 1].classList.contains("black"))
+        {
+            console.debug("Right block is a wall.");
+            checkedIndexes.push(currIndex + 1);
+        }
+
+        else 
+        {
+            console.debug("Checking right block.");
+            setTimeout(checkSurroundings, 100, gridItems, currIndex + 1);
+        }
+    }
+
+    else 
+    {
+        console.debug("Right block is in the rightmost column.");
+    }
+
+    // Checking Up - Similar logic to above
+    if(!(currIndex < 20))
+    {        
+        // Checking up 
+        if (gridItems[currIndex - 20].classList.contains("black"))
+        {
+            console.debug("Upper block is a wall.");
+            checkedIndexes.push(currIndex - 20);
+        }
+
+        else 
+        {
+            console.debug("Checking upper block.");
+            setTimeout(checkSurroundings, 100, gridItems, currIndex - 20);
+        }
+    }
+
+    else 
+    {
+        console.debug("Upward block is in the top row.");
+    }
+
+    // Checking Down  - Similar logic to above
+    if (!(currIndex >= 380))
+    {
+        // Checking down 
+        if (gridItems[currIndex + 20].classList.contains("black"))
+        {
+            console.debug("Downward block is a wall.");
+            checkedIndexes.push(currIndex + 20);
+        }
+
+        else 
+        {
+            console.debug("Checking downward block.");
+            setTimeout(checkSurroundings, 100, gridItems, currIndex + 20);
+        }
+    }
+
+    else 
+    {
+        console.debug("Downward block is in the bottom row.");
+    }
+}
+
+// Returns an index (if a begin block is found) or -1 if none is found 
+function getStartBlockIndex(gridItems)
+{
+    for (let i = 0; i < gridItems.length; i++)
+    {
+        if (gridItems[i].classList.contains("blue"))
+        {
+            console.debug("Found start block at index " + i);
+            return i;
+        }
+    }
+
+    console.debug("Did not find start block.");
+    return -1;
+}
+
+// Returns an index (if an end block is found) or -1 if none is found 
+function getEndBlockIndex(gridItems)
+{
+    for (let i = 0; i < gridItems.length; i++)
+    {
+        if (gridItems[i].classList.contains("red"))
+        {
+            console.debug("Found end block at index " + i);
+            return i;
+        }
+    }
+
+    console.debug("Did not find end block.");
+    return -1;
+}
 
 
 
